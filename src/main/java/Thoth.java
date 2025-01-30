f5166821abf31ae0178c5804f3cbb378c5204e63
Cheng-Zhiyuan <e1138578@u.nus.edu>
2025-01-30 12:06:01 +0800

    Improve Thoth to echo command and exit when user input bye

--- src/main/java/Thoth.java
+++ src/main/java/Thoth.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Thoth {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Thoth");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String userInput = null;
+        while (true) {  
+            userInput = new Scanner(System.in).nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
