1b3cf44b3128d49a2d940a15a0439d392b0b6125
Cheng-Zhiyuan <e1138578@u.nus.edu>
2025-01-30 12:39:35 +0800

    Add the ability to store text entered by the user and display them back when requested.

--- src/main/java/Thoth.java
+++ src/main/java/Thoth.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Thoth {
+    public static final int MAX_TASKS = 100;
+    public static final String INDENT = "%4s";
+
     public static void main(String[] args) {
+
         System.out.println("Hello! I'm Thoth");
         System.out.println("What can I do for you?");
 
-        String userInput = null;
-        while (true) {  
+        String[] taskList = new String[MAX_TASKS];
+        String userInput;
+        int listNumber = 1;
+        while (true) {
             userInput = new Scanner(System.in).nextLine();
             if (userInput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.printf(INDENT + "Bye. Hope to see you again soon!", "");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < listNumber; i++) {
+                    if (taskList[i] != null) {
+                        System.out.printf(INDENT + "%s%n", "", taskList[i]);
+                    }
+                }
             } else {
-                System.out.println(userInput);
+                taskList[listNumber - 1] = listNumber + ". " + userInput;
+                System.out.printf(INDENT + "Added: %s%n", "", userInput);
+                listNumber++;
             }
         }
     }
