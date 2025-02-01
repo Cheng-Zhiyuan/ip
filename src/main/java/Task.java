b0187dc74b3ecdcc9a17ad265863a4a7406cc308
Cheng-Zhiyuan <e1138578@u.nus.edu>
2025-02-01 14:37:46 +0800

    Add a Task class to reperesent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+
+    //parameters for checking and unchecking tasks
+    public static final String EMPTY_BOX = "[ ]";
+    public static final String MARKED_BOX = "[X]";
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getTaskString() {
+        String statusIcon = isDone ? MARKED_BOX : EMPTY_BOX;
+        return statusIcon + " " + description;
+    }
+
+}
