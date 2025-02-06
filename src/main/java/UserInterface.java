b87b63b29285d3e3ca69a7622c4f87e8189ba12a
Cheng-Zhiyuan <e1138578@u.nus.edu>
2025-02-06 20:40:35 +0800

    Add UserInterface class to refactor code

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class UserInterface {
+    private Scanner scanner;
+    public static final String INDENT = "%4s";
+
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String readInput() {
+        return scanner.nextLine();
+    }
+
+    public void printGreetingMessage() {
+        System.out.println("Hello! I'm Thoth");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printGoodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printTask(Task[] task, int taskCount) {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(task[i].toString());
+        }
+    }
 }
